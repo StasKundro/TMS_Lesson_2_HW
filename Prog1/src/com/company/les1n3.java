c5ddf18acdd125145247128bc410dcc3a7b4f088
Stas <88132419+StasKundro@users.noreply.github.com>
2021-07-31 10:13:45 +0300

    Add files via upload

---
+++ Prog1/src/com/company/les1n3.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class les1n3 {
+    public static void main(String[] args) {
+        int a = 0;
+
+            if (a > 0) {
+                a++;
+            } else if(a<0){
+                a-=2;
+            } else if(a==0){
+                a=10;
+            }
+        System.out.println(a);
+
+
+    }
+}
